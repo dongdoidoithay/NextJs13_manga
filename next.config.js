2ffc3d2120cc736a816c576dec36866a9fe03e2c
hainx <xuanhaitk5@gmail.com>
2023-04-02 23:23:29 +0700

    oke fw

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+// stream
+reactMode: 'concurrent', //chưa rõ
+  workerThreads: true,
+  streams: true,
+  pageDataReceived: true,
+  optimizeFonts: true,
+  optimizeImages: true,
+//
+
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
   experimental: {
---
+++ src/constants/baseSeo.ts
@@ -0,0 +1,17 @@
+export default {
+    title:"Read Manga  Heaven |Raw|Scan|Adult|Comics For Free 🦚 1stheavenmanga.xyz",
+    description:"🦚 Read Manga Heaven in website office 1stheavenmanga.xyz.🐳 You can read raw - scan - adult  manga. manhwa, manhua completely free with english interface and many more...",
+    keywords:"Kuma Manga, Manga Kuma, Manga, Read Manga, Manga Online, Read Manga Free, Manga Scans, Free Manga, Read Manga Online, Read Manga Japan",
+    canonical:"https://1stheavenmanga.xyz",
+    domainName:"1st Heaven Manga",
+    archives:'https://1stheavenmanga.xyz',
+    images: [
+        {
+            url: 'https://1stheavenmanga.xyz/image/bg.jpg',
+            width: 800,
+            height: 600,
+            alt: 'Read Manga  Heaven |Raw|Scan|Adult|Comics For Free 🦚 1stheavenmanga.xyz',
+        }
+    ],
+   
+  };
